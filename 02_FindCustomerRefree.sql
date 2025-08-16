253b57f12ef08ab5793c03263b74acbc23b5a212
sachin kumar <goputen215@gmail.com>
2025-08-16 08:15:29 +0530

    First 5 complete

---
+++ 02_FindCustomerRefree.sql
@@ -0,0 +1,24 @@
+-- 584. Find Customer Referee
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+SELECT name FROM customer
+WHERE referee_id != 2 OR referee_id IS NULL;
---
+++ 05_InvalidTweet.sql
@@ -0,0 +1,17 @@
+-- 1683. Invalid Tweets
+-- Input: 
+-- Tweets table:
+-- +----------+-----------------------------------+
+-- | tweet_id | content                           |
+-- +----------+-----------------------------------+
+-- | 1        | Let us Code                       |
+-- | 2        | More than fifteen chars are here! |
+-- +----------+-----------------------------------+
+-- Output: 
+-- +----------+
+-- | tweet_id |
+-- +----------+
+-- | 2        |
+-- +----------+
+SELECT tweet_id FROM tweets
+where LENGTH(content) > 15;
